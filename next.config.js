b63d13d9481bd932099c9dfbd95ca17d4dfc0d0d
hudarashid <hudarashid87@gmail.com>
2023-02-06 21:51:42 +0000

    feat: install typescript

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  reactStrictMode: false,
+  images: {
+    loader: "akamai",
+    path: "",
+  }
 }
 
 module.exports = nextConfig
